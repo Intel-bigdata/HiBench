a34a4b1fd665dc2a8c67117b054a0a5621f60284
dongli3 <dong.d.li@intel.com>
2013-03-05 10:28:00 +0800

    prepare data with dbgen and hadoop streaming, and load to hive Change-Id: If33f84046abc941ed5030fc3ca35af2c0d9e2877

--- tpcds-etl/conf/configure.sh
+++ tpcds-etl/conf/configure.sh
@@ -1,7 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
-DIR=`dirname "$PWD/$0"`
+# dbgen paths
+DBGEN_HOME=/home/hadoop/projects/dbgen
 
-#########
-SCALE=1
-DSGEN_OUTPUT=/tmp/dsgen-output
+# paths
+DBGEN_BASE_HDFS=/HiBench/TPCDS
+DBGEN_INPUT=${DBGEN_BASE_HDFS}/Input
+DBGEN_OUTPUT=${DBGEN_BASE_HDFS}/Output
+DBGEN_DATA=${DBGEN_BASE_HDFS}/DATA
+
+# tables to create
+TABLES_NO_PARALLEL="
+dbgen_version
+date_dim
+time_dim
+call_center
+income_band
+household_demographics
+item
+warehouse
+promotion
+reason
+ship_mode
+store
+web_site
+web_page
+"
+TABLES_PARALLEL="
+customer
+customer_address
+customer_demographics
+inventory
+web_sales
+web_returns
+"
+
+# parameters of dbgen
+SCALE=100
+PARALLEL=128
+TIMEOUT=0
