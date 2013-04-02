22472877a481b079e74ff3c0a06c1d4a86c3540e
dongli3 <dong.d.li@intel.com>
2013-04-02 10:25:59 +0800

    move vars from configure to prepare Change-Id:I9c7710e7ee5dcf451eb353d61db44c3bfbc42e0a

--- etl-sales-db/bin/prepare.sh
+++ etl-sales-db/bin/prepare.sh
@@ -4,56 +4,60 @@ bin=`dirname "$0"`
 bin=`cd "#bin"; pwd`
 
 echo "========== preparing tpcds data =========="
-echo "start configure `date`"
+
 # configure
 DIR=`cd $bin/../; pwd`
 . "${DIR}/../bin/hibench-config.sh"
 . "${DIR}/conf/configure.sh"
-DBGEN_DIR="${DIR}/dbgen"
-echo "start create input `date`"
+DBGEN_LOCAL_DIR="${DIR}/dbgen"
+DBGEN_HDFS_BASE=${ETLRECOMM_HDFS_BASE}/etl-sales-db
+DBGEN_HDFS_INPUT=${DBGEN_HDFS_BASE}/Input
+DBGEN_HDFS_OUTPUT=${DBGEN_HDFS_BASE}/Output
+DBGEN_HDFS_DATA=${DBGEN_HDFS_BASE}/DATA
+
 # prepare mapreduce input
-if [ -d "${DBGEN_DIR}/Input" ]
+if [ -d "${DBGEN_LOCAL_DIR}/Input" ]
 then
-  rm -f ${DBGEN_DIR}/Input/*
+  rm -f ${DBGEN_LOCAL_DIR}/Input/*
 else
-  mkdir ${DBGEN_DIR}/Input
+  mkdir ${DBGEN_LOCAL_DIR}/Input
 fi
 
-for child in $(seq ${PARALLEL});do
-  echo "${child}" > ${DBGEN_DIR}/Input/${child}.txt
+for CHILD in $(seq ${PARALLEL});do
+  echo "${CHILD}" > ${DBGEN_LOCAL_DIR}/Input/${CHILD}.txt
 done
 
-echo "start mkdir `date`"
-${HADOOP_EXECUTABLE} fs -rmr ${DBGEN_BASE_HDFS}
-${HADOOP_EXECUTABLE} fs -Ddfs.umask=0000 -mkdir ${DBGEN_BASE_HDFS}
-${HADOOP_EXECUTABLE} fs -moveFromLocal ${DBGEN_DIR}/Input ${DBGEN_BASE_HDFS}
-${HADOOP_EXECUTABLE} fs -Ddfs.umask=0000 -mkdir ${DBGEN_DATA}
+echo $DBGEN_HDFS_DATA
 
-TMP_DIR_STR=""
-for tnp in $TABLES_NO_PARALLEL ; do
-  ${HADOOP_EXECUTABLE} fs -Ddfs.umask=0000 -mkdir ${DBGEN_DATA}/${tnp} &
-done
+${HADOOP_EXECUTABLE} fs -rmr ${DBGEN_HDFS_BASE}
+${HADOOP_EXECUTABLE} fs -Ddfs.umask=0000 -mkdir ${DBGEN_HDFS_DATA}
+${HADOOP_EXECUTABLE} fs -moveFromLocal ${DBGEN_LOCAL_DIR}/Input ${DBGEN_HDFS_INPUT}
 
-for tp in $TABLES_PARALLEL ; do
-  ${HADOOP_EXECUTABLE} fs -Ddfs.umask=0000 -mkdir ${DBGEN_DATA}/${tp} &
-done
-
-for rt in s_customer s_customer_address s_inventory s_item s_promotion delete s_web_order s_web_order_lineitem s_web_page s_web_returns s_web_site s_call_center s_warehouse s_zip_to_gmt
+for TNP in dbgen_version date_dim time_dim call_center income_band household_demographics item warehouse promotion reason ship_mode store web_site web_page 
 do
-  ${HADOOP_EXECUTABLE} fs -Ddfs.umask=0000 -mkdir ${DBGEN_DATA}/${rt} &
+  ${HADOOP_EXECUTABLE} fs -Ddfs.umask=0000 -mkdir ${DBGEN_HDFS_DATA}/${TNP} &
+done
+
+for TP in customer customer_address customer_demographics inventory web_sales web_returns
+do
+  ${HADOOP_EXECUTABLE} fs -Ddfs.umask=0000 -mkdir ${DBGEN_HDFS_DATA}/${TP} &
+done
+
+for RT in s_customer s_customer_address s_inventory s_item s_promotion delete s_web_order s_web_order_lineitem s_web_page s_web_returns s_web_site s_call_center s_warehouse s_zip_to_gmt
+do
+  ${HADOOP_EXECUTABLE} fs -Ddfs.umask=0000 -mkdir ${DBGEN_HDFS_DATA}/${RT} &
 done
 wait
 
-echo "start mapreduce create data file and upload to hdfs `date`"
 # generate data
-cd ${DBGEN_DIR}
-OPTION="-input ${DBGEN_INPUT} \
--output ${DBGEN_OUTPUT} \
+cd ${DBGEN_LOCAL_DIR}
+OPTION="-input ${DBGEN_HDFS_INPUT} \
+-output ${DBGEN_HDFS_OUTPUT} \
 -mapper ./run.sh \
 -file ${DBGEN_HOME}/tools/dsdgen -file run.sh -file ${DBGEN_HOME}/tools/tpcds.idx -file ${DIR}/conf/configure.sh \
 -jobconf mapred.reduce.tasks=0 \
 -jobconf mapred.job.name=prepare_etl_sales_db \
--jobconf mapred.task.timeout=${TIMEOUT}"
+-jobconf mapred.task.timeout=0"
 
 # export streaming
 if [ -z "$STREAMING" ]; then
@@ -61,21 +65,18 @@ if [ -z "$STREAMING" ]; then
 fi
 
 START_TIME=`timestamp`
+
 # echo "start generating data"
 ${HADOOP_EXECUTABLE} jar ${STREAMING} ${OPTION}
 
-echo "start loading to hive `date`"
 # Create tables
-sed "s#ETL_SALES_DB_DIR#${DBGEN_DATA}#g" $DIR/bin/hive/create_base.template > $DIR/bin/hive/create_base.hive
+sed "s#ETL_SALES_DB_DIR#${DBGEN_HDFS_DATA}#g" $DIR/bin/hive/create_base.template > $DIR/bin/hive/create_base.hive
 cd ${DIR}/bin
 $HIVE_HOME/bin/hive -f $DIR/bin/hive/create_base.hive
 echo "start loading refresh data `date`"
+
 # Create refresh tables
-sed "s#ETL_SALES_DB_DIR#${DBGEN_DATA}#g" $DIR/bin/hive/create_refresh.template > $DIR/bin/hive/create_refresh.hive
-pwd
+sed "s#ETL_SALES_DB_DIR#${DBGEN_HDFS_DATA}#g" $DIR/bin/hive/create_refresh.template > $DIR/bin/hive/create_refresh.hive
 $HIVE_HOME/bin/hive -f $DIR/bin/hive/create_refresh.hive
 
-echo "all finish `date`"
 END_TIME=`timestamp`
-
-# echo "all finished"
--- etl-sales-db/conf/configure.sh
+++ etl-sales-db/conf/configure.sh
@@ -4,38 +4,8 @@
 DBGEN_HOME=/home/hadoop/projects/dbgen
 
 # paths
-DBGEN_BASE_HDFS=/HiBench/etl_sales_db
-DBGEN_INPUT=${DBGEN_BASE_HDFS}/Input
-DBGEN_OUTPUT=${DBGEN_BASE_HDFS}/Output
-DBGEN_DATA=${DBGEN_BASE_HDFS}/DATA
-
-# tables to create
-TABLES_NO_PARALLEL="
-dbgen_version
-date_dim
-time_dim
-call_center
-income_band
-household_demographics
-item
-warehouse
-promotion
-reason
-ship_mode
-store
-web_site
-web_page
-"
-TABLES_PARALLEL="
-customer
-customer_address
-customer_demographics
-inventory
-web_sales
-web_returns
-"
+ETLRECOMM_HDFS_BASE=${DATA_HDFS}/etl-recomm
 
 # parameters of dbgen
 SCALE=100
 PARALLEL=128
-TIMEOUT=0
