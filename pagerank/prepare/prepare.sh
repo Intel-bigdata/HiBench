16d8e0224c5b9173ab8bc9e331254a96ed8e6614
Lv, Qi <qi.lv@intel.com>
2014-08-05 10:29:59 +0800

    import kmeans & pagerank

---
+++ pagerank/prepare/prepare.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -u
+
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+
+echo "========== preparing pagerank data=========="
+# configure
+DIR=`cd $bin/../; pwd`
+. "${DIR}/../bin/hibench-config.sh"
+. "${DIR}/conf/configure.sh"
+
+# compress check
+# compress
+if [ $COMPRESS -eq 1 ]; then
+    COMPRESS_OPT="-c ${COMPRESS_CODEC}"
+else
+    COMPRESS_OPT=""
+fi
+
+# path check
+$HADOOP_EXECUTABLE dfs -rmr $INPUT_HDFS
+
+# generate data
+#DELIMITER=\t
+OPTION="-t pagerank \
+        -b ${PAGERANK_BASE_HDFS} \
+        -n ${PAGERANK_INPUT} \
+        -m ${NUM_MAPS} \
+        -r ${NUM_REDS} \
+        -p ${PAGES} \
+        -o text"
+#       -d ${DELIMITER} \
+echo $HADOOP_EXECUTABLE jar ${DATATOOLS} HiBench.DataGen ${OPTION} ${COMPRESS_OPT}
+$HADOOP_EXECUTABLE jar ${DATATOOLS} HiBench.DataGen ${OPTION} ${COMPRESS_OPT}
+result=$?
+if [ $result -ne 0 ]
+then
+    echo "ERROR: Hadoop job failed to run successfully." 
+    exit $result
+fi
---
+++ pagerank/python/bin/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -u
+
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+
+echo "========== running pagerank bench =========="
+# configure
+DIR=`cd $bin/../; pwd`
+. "${DIR}/../../bin/hibench-config.sh"
+. "${DIR}/../conf/configure.sh"
+
+# compress
+
+# path check
+#$HADOOP_EXECUTABLE dfs -rmr  $OUTPUT_HDFS
+
+# pre-running
+#SIZE=$($HADOOP_EXECUTABLE job -history $INPUT_HDFS | grep 'org.apache.hadoop.examples.RandomTextWriter$Counters.*|BYTES_WRITTEN')
+#SIZE=${SIZE##*|}
+#SIZE=${SIZE//,/}
+START_TIME=`timestamp`
+
+# run bench
+echo $SPARK_HOME
+$SPARK_HOME/bin/spark-submit --master ${SPARK_MASTER} ${DIR}/pagerank.py $INPUT_HDFS $NUM_ITERATIONS
+
+# post-running
+END_TIME=`timestamp`
+#gen_report "WORDCOUNT" ${START_TIME} ${END_TIME} ${SIZE}
