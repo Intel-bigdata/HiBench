f20367f8321ef5b4a4c4bc7608dba63cd48d8d9f
Neo Qian <shilei.qian@gmail.com>
2014-08-06 15:19:42 +0800

    Add maven to nutch

---
+++ common/hibench/src/main/java/com/intel/hibench/App.java
@@ -0,0 +1,13 @@
+package com.intel.hibench;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
