66a032cccdbdaa17eb1bb17d5051a4479f68892e
Daoyuan Wang <daoyuan.wang@intel.com>
2014-08-07 14:48:54 +0800

    apply jayunit's patch to yarn

--- bayes/bin/run.sh
+++ bayes/bin/run.sh
@@ -39,8 +39,10 @@ fi
 START_TIME=`timestamp`
 
 # run bench
+# Note that we borrow the numReducers parameter from the environment.  It must be set or else you will only run
+# one reducer.
 $MAHOUT_HOME/bin/mahout seq2sparse \
-        $COMPRESS_OPT -i ${INPUT_HDFS} -o ${OUTPUT_HDFS}/vectors  -lnorm -nv  -wt tfidf -ng ${NGRAMS}
+        $COMPRESS_OPT -i ${INPUT_HDFS} -o ${OUTPUT_HDFS}/vectors  -lnorm -nv  -wt tfidf -ng ${NGRAMS} --numReducers $NUM_REDS
 
 $MAHOUT_HOME/bin/mahout trainnb \
         $COMPRESS_OPT -i ${OUTPUT_HDFS}/vectors/tfidf-vectors -el -o ${OUTPUT_HDFS}/model -li ${OUTPUT_HDFS}/labelindex  -ow --tempDir ${OUTPUT_HDFS}/temp
