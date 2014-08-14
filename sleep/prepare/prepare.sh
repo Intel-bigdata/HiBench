894f31733f1b6abede24a6c23a58f05115a13607
Lv, Qi <qi.lv@intel.com>
2014-08-14 16:07:30 +0800

    sleep code done

---
+++ sleep/prepare/prepare.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -u
+
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+
+echo "========== preparing sleep =========="
+# configure
+DIR=`cd $bin/../; pwd`
+. "${DIR}/../bin/hibench-config.sh"
+. "${DIR}/conf/configure.sh"
+
+# compress check
+if [ $COMPRESS -eq 1 ]; then
+    COMPRESS_OPT="-D mapred.output.compress=true \
+    -D mapred.output.compression.codec=$COMPRESS_CODEC \
+    -D mapred.output.compression.type=BLOCK "
+else
+    COMPRESS_OPT="-D mapred.output.compress=false"
+fi
+
+# path check
+$HADOOP_EXECUTABLE dfs -rmr $INPUT_HDFS
+
+# generate data
---
+++ sleep/scala/src/main/scala/ScalaSleep.scala
@@ -0,0 +1,24 @@
+
+import org.apache.spark.rdd._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object ScalaSort{
+  def main(args: Array[String]){
+    if (args.length < 2){
+      System.err.println(
+        s"Usage: $ScalaSleep <parallel> <seconds>"
+      )
+      System.exit(1)
+    }
+    val sparkConf = new SparkConf().setAppName("ScalaSleep")//.setMaster("local[2]")
+    val sc = new SparkContext(sparkConf)
+
+    val parallel = args(0).toInt
+    val seconds  = args(1).toInt
+    val workload = sc.parallelize(1 to parallel).map(x=> Thread.sleep(seconds * 1000L))
+    workload.collect();
+    sc.stop()
+  }
+}
