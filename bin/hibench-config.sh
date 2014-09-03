8e5231b8efd5324df8e3078ea77fcd5baaf2700e
Neo Qian <shilei.qian@gmail.com>
2014-09-03 15:32:07 +0800

    Improve a little things

--- bin/hibench-config.sh
+++ bin/hibench-config.sh
@@ -76,7 +76,7 @@ if [ -f "${HIBENCH_CONF}/funcs.sh" ]; then
 fi
 
 if [ -z "$HIVE_HOME" ]; then
-    export HIVE_HOME=${HIBENCH_HOME}/common/hive-0.12.0-bin
+    export HIVE_HOME=${HIBENCH_HOME}/common/hibench/hivebench/target/hive-0.12.0-bin
 fi
 
 if [ -z "$MAHOUT_HOME" ]; then
@@ -84,7 +84,7 @@ if [ -z "$MAHOUT_HOME" ]; then
 fi
 
 if [ -z "$NUTCH_HOME" ]; then
-    export NUTCH_HOME=${HIBENCH_HOME}/nutchindexing/nutch
+    export NUTCH_HOME=${HIBENCH_HOME}/common/hibench/nutchindexing/target/nutch-1.2
 fi
 
 if [ -z "$DATATOOLS" ]; then
