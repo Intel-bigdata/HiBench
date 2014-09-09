23d53e5a0fbacd7ae23fd6575f6d19f8e8053ade
Lv, Qi <qi.lv@intel.com>
2014-09-09 19:53:48 +0800

    improve bayes converter

--- bayes/prepare/src/main/scala/Convert.scala
+++ bayes/prepare/src/main/scala/Convert.scala
@@ -1,7 +1,6 @@
 import java.io._
 
 import org.apache.hadoop.conf._
-import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs._
 import org.apache.hadoop.io._
 import org.apache.hadoop.util.ReflectionUtils
@@ -17,42 +16,48 @@ import org.apache.spark.SparkContext._
 object Convert{
   val conf = new Configuration()
   def main(args: Array[String]){
-    if (args.length!=3){
-      System.err.println("Usage: Convert <hdfs_master> <input_path> <PARALLEL>")
+    if (args.length!=2){
+      System.err.println("Usage: Convert <input_path> <PARALLEL>")
       System.exit(1)
     }
 
-    val hdfs_master =  args(0)  //"hdfs://localhost:54310/"
-    val input_path =   args(1)  //"hdfs://localhost:54310/HiBench/Bayes/Input"
-    val parallel   = args(2).toInt
-    val output_name =  input_path +"/samples.txt"
+//    val hdfs_master =  args(0)  //"hdfs://localhost:54310/"
+    val input_path =   args(0)  //"hdfs://localhost:54310/HiBench/Bayes/Input"
+    val parallel   = args(1).toInt
+//    val output_name =  input_path +"/samples.txt"
     val output_vector_name = input_path + "/vectors.txt"
-    val hdfs_head = if (hdfs_master.endsWith("/"))  hdfs_master.length - 1 else hdfs_master.length
 
-    conf.setStrings("fs.default.name", hdfs_master)
-    conf.setStrings("dfs.replication", "1")
+//    val hdfs_head = if (hdfs_master.endsWith("/"))  hdfs_master.length - 1 else hdfs_master.length
 
-    val fileSystem = FileSystem.get(conf)
-    val hdfs_input_path = output_name.substring(hdfs_head)
-    println(s"$hdfs_input_path, $hdfs_master, $input_path, $output_name")
-    val out = fileSystem.create(new Path(output_name.substring(hdfs_head))) //new BufferedWriter(new OutputStreamWriter(fileSystem.create(new Path(output_name))))
+//    conf.setStrings("fs.default.name", hdfs_master)
+//    conf.setStrings("dfs.replication", "1")
 
-    val dirs = fileSystem.listStatus(new Path(input_path.substring(hdfs_head)))
-    dirs.foreach { it =>
-      if (it.getPath.getName.startsWith("part-")) {
-        println("Processing file %s".format(it.getPath))
-        IterTextRecordInputStream(fileSystem, it.getPath, conf, out)
-      }
-    }
-    out.close()
+//    val fileSystem = FileSystem.get(conf)
+//    val hdfs_input_path = output_name.substring(hdfs_head)
+//    println(s"$hdfs_input_path, $hdfs_master, $input_path, $output_name")
+//    val out = fileSystem.create(new Path(output_name.substring(hdfs_head))) //new BufferedWriter(new OutputStreamWriter(fileSystem.create(new Path(output_name))))
 
-    // calc word frequency
     val sparkConf = new SparkConf().setAppName("Doc2Vector")
     val sc = new SparkContext(sparkConf)
-    val data = sc.textFile(output_name).map({ line =>
-      val x = line.split(":")
-      (x(0), x(1))
-    }) // map line to key:data
+
+    val data = sc.sequenceFile[BytesWritable, BytesWritable](input_path).map{case(k, v) => (k.toString, v.toString)}
+
+//    val dirs = fileSystem.listStatus(new Path(input_path.substring(hdfs_head)))
+//    dirs.foreach { it =>
+//      if (it.getPath.getName.startsWith("part-")) {
+//        println("Processing file %s".format(it.getPath))
+//        IterTextRecordInputStream(fileSystem, it.getPath, conf, out)
+//      }
+//    }
+//    out.close()
+
+    // calc word frequency
+    
+//    val data = sc.textFile(output_name).map({ line =>
+//      val x = line.split(":")
+//      (x(0), x(1))
+//    }) // map line to key:data
+
     data.repartition(parallel)
     val wordcount = data.flatMap{case(key, doc) => doc.split(" ")}
                         .map(word => (word, 1))
@@ -80,24 +85,10 @@ object Convert{
     }
 //    println(vector.count())
 //    println("delete:" + output_vector_name.substring(hdfs_head))
-    try { fileSystem.delete(new Path(output_vector_name.substring(hdfs_head)), true) } catch { case _ : Throwable => { } }
-    vector.saveAsTextFile(output_vector_name)
-    fileSystem.close()
+//    try { fileSystem.delete(new Path(output_vector_name.substring(hdfs_head)), true) } catch { case _ : Throwable => { } }
+//    vector.saveAsTextFile(output_vector_name)
+//    fileSystem.close()
     sc.stop()
-  }
-
-  def IterTextRecordInputStream(fs: FileSystem, path:Path, conf:Configuration, out:FSDataOutputStream):Int = {
-    var r: SequenceFile.Reader = new SequenceFile.Reader(fs, path, conf)
-    var key:Writable = ReflectionUtils.newInstance(r.getKeyClass.asSubclass(classOf[Writable]), conf)
-    var `val`:Writable = ReflectionUtils.newInstance(r.getValueClass.asSubclass(classOf[Writable]), conf)
-
-    var vector:Array[Double] = null
-    var size = -1
-    while (true){
-      if (!r.next(key, `val`)) return -1
-      out.write("%s:%s\n".format(key, `val`).getBytes)
-    }
-    0
   }
 }
 
