fa8ada41271ea2d7b8a8814cc6c11ffe5579e698
Lv, Qi <qi.lv@intel.com>
2014-09-09 15:05:11 +0800

    fixing

--- kmeans/prepare/src/main/scala/Convert.scala
+++ kmeans/prepare/src/main/scala/Convert.scala
@@ -7,36 +7,48 @@ import org.apache.hadoop.io._
 import org.apache.hadoop.util.ReflectionUtils
 import org.apache.mahout.math.VectorWritable
 import org.apache.mahout.math.Vector
+import org.apache.spark._
+import org.apache.spark.rdd._
+import org.apache.spark.SparkContext._
 
 
 
 object Convert{
   val conf = new Configuration()
   def main(args: Array[String]){
-    if (args.length!=4){
-      System.err.println("Usage: Convert <hdfs_master> <input_directory> <output_file_path> <PARALLEL>")
-      System.exit(1)
+//    if (args.length!=4){
+//      System.err.println("Usage: Convert <hdfs_master> <input_directory> <output_file_path> <PARALLEL>")
+//      System.exit(1)
+//    }
+
+    val sparkConf = new SparkConf().setAppName("Doc2Vector")
+    val sc = new SparkContext(sparkConf)
+
+//    val hdfs_master =  args(0) //"hdfs://localhost:54310/"
+    val input_path =   "hdfs://localhost:54310/SparkBench/KMeans/Input/samples/"
+//    val output_name =  args(2) //"/HiBench/KMeans/Input/samples.txt"
+//    val parallel = args(3).toInt
+
+    val data = sc.textFile(input_path)
+    data.foreach{line=>
+      println(line)
     }
-
-    val hdfs_master =  args(0) //"hdfs://localhost:54310/"
-    val input_path =   args(1) //"/HiBench/KMeans/Input/samples/"
-    val output_name =  args(2) //"/HiBench/KMeans/Input/samples.txt"
-    val parallel = args(3).toInt
-
-    conf.setStrings("fs.default.name", hdfs_master)
-    conf.setStrings("dfs.replication", "1")
-
-    val fileSystem = FileSystem.get(conf)
-    val out = fileSystem.create(new Path(output_name)) //new BufferedWriter(new OutputStreamWriter(fileSystem.create(new Path(output_name))))
-
-    val dirs = fileSystem.listStatus(new Path(input_path))
-    dirs.foreach { it =>
-      if (it.getPath.getName.startsWith("part-")) {
-        println("Processing file %s".format(it.getPath))
-        IterTextRecordInputStream(fileSystem, it.getPath, conf, out)
-      }
-    }
-    out.close()
+    sc.stop()
+//
+//    conf.setStrings("fs.default.name", hdfs_master)
+//    conf.setStrings("dfs.replication", "1")
+//
+//    val fileSystem = FileSystem.get(conf)
+//    val out = fileSystem.create(new Path(output_name)) //new BufferedWriter(new OutputStreamWriter(fileSystem.create(new Path(output_name))))
+//
+//    val dirs = fileSystem.listStatus(new Path(input_path))
+//    dirs.foreach { it =>
+//      if (it.getPath.getName.startsWith("part-")) {
+//        println("Processing file %s".format(it.getPath))
+//        IterTextRecordInputStream(fileSystem, it.getPath, conf, out)
+//      }
+//    }
+//    out.close()
   }
 
   def IterTextRecordInputStream(fs: FileSystem, path:Path, conf:Configuration, out:FSDataOutputStream):Int = {
