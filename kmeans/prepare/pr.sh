7a7f3507377ac3c512937a271347fe7883d5abf9
Lv, Qi <qi.lv@intel.com>
2014-09-09 15:06:15 +0800

    add pr

---
+++ kmeans/prepare/pr.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+
+echo "========== preparing kmeans data =========="
+# configure
+DIR=`cd $bin/../; pwd`
+. "${DIR}/../bin/sparkbench-config.sh"
+. "${DIR}/conf/configure.sh"
+
+
+# compress check
+if [ $COMPRESS -eq 1 ]; then
+    COMPRESS_OPT="-compress true \
+        -compressCodec $COMPRESS_CODEC \
+        -compressType BLOCK "
+else
+    COMPRESS_OPT="-compress false"
+fi
+
+# paths check
+
+# generate data
+${SPARK_HOME}/bin/spark-submit --class Convert --master local ${DIR}/prepare/target/scala-2.10/hibench-kmean-converter_2.10-1.0.jar ${HDFS_MASTER} ${INPUT_HDFS} ${PARALLEL}
+#( cd `dirname $0` && sbt "run ${HDFS_MASTER} ${INPUT_SAMPLE} ${INPUT_HDFS} ${PARALLEL}" )
+result=$?
+if [ $result -ne 0 ]
+then
+    echo "ERROR: covert job failed to run successfully." 
+    exit $result
+fi
+
