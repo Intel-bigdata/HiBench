81b99263d653f4d5626c71e13b8bc13848006476
Lv, Qi <qi.lv@intel.com>
2014-09-28 16:53:45 +0800

    add configurable ordered rdd implementation

--- common/src/main/scala/com/intel/sparkbench/ScalaTeraSort.scala
+++ common/src/main/scala/com/intel/sparkbench/ScalaTeraSort.scala
@@ -17,34 +17,14 @@
 
 package com.intel.sparkbench.terasort
 
+import com.intel.sparkbench.{ConfigurableRangePartitioner, ConfigurableOrderedRDDFunctions}
 import org.apache.spark.rdd._
 import org.apache.spark._
 import scala.reflect.ClassTag
 
-class SampleRangePartitioner [K : Ordering : ClassTag, V](
-         @transient partitions: Int,
-         @transient rdd: RDD[_ <: Product2[K,V]],
-         private var ascending: Boolean = true)
-  extends RangePartitioner[K, V](partitions, rdd, ascending) {
-
-
-  }
-class SampledOrderedRDDFunctions[K : Ordering : ClassTag, V: ClassTag,
-                         P <: Product2[K, V] : ClassTag]
-                        (self: RDD[P]) extends Logging with Serializable {
-  private val ordering = implicitly[Ordering[K]]
-
-  def sortByKey(ascending: Boolean = true, numPartitions: Int = self.partitions.size): RDD[(K, V)] =
-  {
-    val part = new RangePartitioner(numPartitions, self, ascending)
-    new ShuffledRDD[K, V, V](self, part)
-      .setKeyOrdering(if (ascending) ordering else ordering.reverse)
-  }
-}
-
 object ScalaTeraSort{
   implicit def rddToSampledOrderedRDDFunctions[K : Ordering : ClassTag, V: ClassTag]
-     (rdd: RDD[(K, V)]) = new SampledOrderedRDDFunctions[K, V, (K, V)](rdd)
+     (rdd: RDD[(K, V)]) = new ConfigurableOrderedRDDFunctions[K, V, (K, V)](rdd)
 
   def main(args: Array[String]){
     if (args.length != 2){
@@ -57,9 +37,11 @@ object ScalaTeraSort{
     val sc = new SparkContext(sparkConf)
 
     val file = sc.textFile(args(0))
-    val parallel = sc.getConf.getInt("spark.default.parallelism", 0)
+    val parallel = sc.getConf.getInt("spark.default.parallelism", sc.defaultParallelism)
     val data = file.map(line => (line.substring(0, 10), line.substring(10)))
-      .sortByKey(numPartitions=parallel).map{case(k, v) => k + v}
+
+    val partitioner = new BaseRangePartitioner(partitions = parallel / 2, data);
+    val sorted_data = data.sortByKeyWithPartitioner(partitioner = partitioner).map{case(k, v) => k + v}
     data.saveAsTextFile(args(1))
 
     sc.stop()
---
+++ common/src/main/scala/org/apache/spark/BaseRangePartitioner.scala
@@ -0,0 +1,159 @@
+package org.apache.spark
+
+import java.io.{IOException, ObjectInputStream, ObjectOutputStream}
+
+import org.apache.spark.rdd.{PartitionPruningRDD, RDD}
+import org.apache.spark.serializer.JavaSerializer
+import org.apache.spark.util.{CollectionsUtils, Utils}
+
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.reflect.ClassTag
+import scala.util.hashing._
+
+class BaseRangePartitioner [K : Ordering : ClassTag, V]
+      (@transient partitions: Int,
+       @transient rdd: RDD[_ <: Product2[K,V]],
+       private var ascending: Boolean = true)
+  extends Partitioner {   // Adopted from scala's RangePartitioner
+  
+  // We allow partitions = 0, which happens when sorting an empty RDD under the default settings.
+  require(partitions >= 0, s"Number of partitions cannot be negative but found $partitions.")
+
+  protected var ordering = implicitly[Ordering[K]]
+
+  // An array of upper bounds for the first (partitions - 1) partitions
+  protected var rangeBounds: Array[K] = {
+    if (partitions <= 1) {
+      Array.empty
+    } else {
+      val requiredPartitions = if (rdd.partitions.size > partitions) partitions else rdd.partitions.size
+      val samplePerPartition = math.min(partitions / requiredPartitions * 20, 1e3).toInt
+
+      // first, sample over partitions averagely
+      rdd.map(_._1).repartition()
+
+
+      // randomly sample over all partitions
+      // This is the sample size we need to have roughly balanced output partitions, capped at 1M.
+      val sampleSize = math.min(20.0 * partitions, 1e6)
+      // Assume the input partitions are roughly balanced and over-sample a little bit.
+      val sampleSizePerPartition = math.ceil(3.0 * sampleSize / rdd.partitions.size).toInt
+      val (numItems, sketched) = RangePartitioner.sketch(rdd.map(_._1), sampleSizePerPartition)
+      if (numItems == 0L) {
+        Array.empty
+      } else {
+        // If a partition contains much more than the average number of items, we re-sample from it
+        // to ensure that enough items are collected from that partition.
+        val fraction = math.min(sampleSize / math.max(numItems, 1L), 1.0)
+        val candidates = ArrayBuffer.empty[(K, Float)]
+        val imbalancedPartitions = mutable.Set.empty[Int]
+        sketched.foreach { case (idx, n, sample) =>
+          if (fraction * n > sampleSizePerPartition) {
+            imbalancedPartitions += idx
+          } else {
+            // The weight is 1 over the sampling probability.
+            val weight = (n.toDouble / sample.size).toFloat
+            for (key <- sample) {
+              candidates += ((key, weight))
+            }
+          }
+        }
+        if (imbalancedPartitions.nonEmpty) {
+          // Re-sample imbalanced partitions with the desired sampling probability.
+          val imbalanced = new PartitionPruningRDD(rdd.map(_._1), imbalancedPartitions.contains)
+          val seed = byteswap32(-rdd.id - 1)
+          val reSampled = imbalanced.sample(withReplacement = false, fraction, seed).collect()
+          val weight = (1.0 / fraction).toFloat
+          candidates ++= reSampled.map(x => (x, weight))
+        }
+        RangePartitioner.determineBounds(candidates, partitions)
+      }
+    }
+  }
+
+  def numPartitions = rangeBounds.length + 1
+
+  protected var binarySearch: ((Array[K], K) => Int) = CollectionsUtils.makeBinarySearch[K]
+
+  def getPartition(key: Any): Int = {
+    val k = key.asInstanceOf[K]
+    var partition = 0
+    if (rangeBounds.length <= 128) {
+      // If we have less than 128 partitions naive search
+      while (partition < rangeBounds.length && ordering.gt(k, rangeBounds(partition))) {
+        partition += 1
+      }
+    } else {
+      // Determine which binary search method to use only once.
+      partition = binarySearch(rangeBounds, k)
+      // binarySearch either returns the match location or -[insertion point]-1
+      if (partition < 0) {
+        partition = -partition-1
+      }
+      if (partition > rangeBounds.length) {
+        partition = rangeBounds.length
+      }
+    }
+    if (ascending) {
+      partition
+    } else {
+      rangeBounds.length - partition
+    }
+  }
+
+  override def equals(other: Any): Boolean = other match {
+    case r: BaseRangePartitioner[_,_] =>
+      r.rangeBounds.sameElements(rangeBounds) && r.ascending == ascending
+    case _ =>
+      false
+  }
+
+  override def hashCode(): Int = {
+    val prime = 31
+    var result = 1
+    var i = 0
+    while (i < rangeBounds.length) {
+      result = prime * result + rangeBounds(i).hashCode
+      i += 1
+    }
+    result = prime * result + ascending.hashCode
+    result
+  }
+
+  @throws(classOf[IOException])
+  protected def writeObject(out: ObjectOutputStream) {
+    val sfactory = SparkEnv.get.serializer
+    sfactory match {
+      case js: JavaSerializer => out.defaultWriteObject()
+      case _ =>
+        out.writeBoolean(ascending)
+        out.writeObject(ordering)
+        out.writeObject(binarySearch)
+
+        val ser = sfactory.newInstance()
+        Utils.serializeViaNestedStream(out, ser) { stream =>
+          stream.writeObject(scala.reflect.classTag[Array[K]])
+          stream.writeObject(rangeBounds)
+        }
+    }
+  }
+
+  @throws(classOf[IOException])
+  protected def readObject(in: ObjectInputStream) {
+    val sfactory = SparkEnv.get.serializer
+    sfactory match {
+      case js: JavaSerializer => in.defaultReadObject()
+      case _ =>
+        ascending = in.readBoolean()
+        ordering = in.readObject().asInstanceOf[Ordering[K]]
+        binarySearch = in.readObject().asInstanceOf[(Array[K], K) => Int]
+
+        val ser = sfactory.newInstance()
+        Utils.deserializeViaNestedStream(in, ser) { ds =>
+          implicit val classTag = ds.readObject[ClassTag[Array[K]]]()
+          rangeBounds = ds.readObject[Array[K]]()
+        }
+    }
+  }
+}
