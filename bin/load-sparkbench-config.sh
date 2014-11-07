e7e8dab67f66c9304a3bf9da69c2d1c8834b8879
Lv, Qi <qi.lv@intel.com>
2014-11-07 16:57:22 +0800

    minor bug fix after PR merge

--- bin/load-sparkbench-config.sh
+++ bin/load-sparkbench-config.sh
@@ -26,17 +26,16 @@ if [[ ! -e "$SPARKBENCH_CONF" ]]; then
 exit
 fi
 
-# local report
-SPARKBENCH_REPORT=${SPARKBENCH_HOME}/sparkbench.report
-
-# default Mappers and reducers
-NUM_MAPS=`grep spark.default.parallelism $SPARKBENCH_HOME/conf/global_properties.conf | tr -s ' ' |cut -d ' ' -f 2`
-NUM_MAPS=${NUM_MAPS:-16}  # default parallelism is 16 if not defined in "global_properties.conf"
-NUM_REDS=$(( $NUM_MAPS/2 ))
-
 if [ -f "$SPARKBENCH_CONF/sparkbench-config.sh" ]; then
 	. "$SPARKBENCH_CONF/sparkbench-config.sh"
 fi
+
+# default Mappers and reducers
+NUM_MAPS_default_parallelism=`grep spark.default.parallelism $SPARKBENCH_HOME/conf/global_properties.conf | tr -s ' ' |cut -d ' ' -f 2`
+NUM_MAPS=${NUM_MAPS:-$NUM_MAPS_default_parallelism}
+NUM_MAPS=${NUM_MAPS:-16}  # default parallelism is 16 if not defined in "global_properties.conf" and "sparkbench-config.sh"
+NUM_REDS=${NUM_REDS:$(( $NUM_MAPS/2 ))}
+
 
 if [ -n "$HADOOP_HOME" ]; then
 	HADOOP_EXECUTABLE=$HADOOP_HOME/bin/hadoop
