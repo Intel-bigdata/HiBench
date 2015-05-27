19a8d5bcb4a68a46e95ef2d59b9719806fdc3d41
Lv, Qi <qi.lv@intel.com>
2015-05-27 14:23:29 +0800

    redirect samza's script & workload improvements

--- src/streambench/stormbench/src/jvm/com/intel/PRCcloud/micro/Wordcount.java
+++ src/streambench/stormbench/src/jvm/com/intel/PRCcloud/micro/Wordcount.java
@@ -13,36 +13,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Wordcount extends SingleSpoutTops{
-  
-  public Wordcount(StormBenchConfig config){
-    super(config);
-  }
 
-  public void setBolt(TopologyBuilder builder){
-          builder.setBolt("split",new SplitStreamBolt(config.separator),config.boltThreads/2).shuffleGrouping("spout");
-          builder.setBolt("count",new WordCountBolt(), config.boltThreads/2).fieldsGrouping("split", new Fields("word"));
-  }
-
-  public static class WordCountBolt extends BaseBasicBolt {
-    Map<String, Integer> counts = new HashMap<String, Integer>();
-
-    @Override
-    public void execute(Tuple tuple, BasicOutputCollector collector){
-      String word = tuple.getString(0);
-      Integer count = counts.get(word);
-      if (count == null)
-        count = 0;
-      count++;
-      counts.put(word, count);
-      //BenchLogUtil.logMsg("Word:"+word+"  count:"+count);
-      collector.emit(new Values(word, count));
-        //FIXME: does this implementation count words globally?
+    public Wordcount(StormBenchConfig config){
+        super(config);
     }
 
-    @Override
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
-      declarer.declare(new Fields("word", "count"));
+    public void setBolt(TopologyBuilder builder){
+        builder.setBolt("split",new SplitStreamBolt(config.separator),config.boltThreads/2).shuffleGrouping("spout");
+        builder.setBolt("count",new WordCountBolt(), config.boltThreads/2).fieldsGrouping("split", new Fields("word"));
     }
-  }
+
+    public static class WordCountBolt extends BaseBasicBolt {
+        Map<String, Integer> counts = new HashMap<String, Integer>();
+
+        @Override
+        public void execute(Tuple tuple, BasicOutputCollector collector){
+            String word = tuple.getString(0);
+            Integer count = counts.get(word);
+            if (count == null)
+                count = 0;
+            count++;
+            counts.put(word, count);
+            //BenchLogUtil.logMsg("Word:"+word+"  count:"+count);
+            collector.emit(new Values(word, count));
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("word", "count"));
+        }
+    }
 
 }
---
+++ workloads/streamingbench/samza/bin/run.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -u
+bin=`dirname "$0"`
+bin=`cd "$bin";pwd`
+DIR=`cd $bin/../; pwd`
+SRC_DIR="$DIR/../../../src/streambench/samza"
+. "${SRC_DIR}/conf/configure.sh"
+echo "=========start samza benchmark $benchName========="
+
+if [ ! -d $SRC_DIR/target ]; then
+  echo "Please run maven first."
+  exit 1
+fi
+
+if [ ! -d $SRC_DIR/target/samza ]; then
+  mkdir $SRC_DIR/target/samza
+  tar zxf $SRC_DIR/target/*.tar.gz -C $SRC_DIR/target/samza
+fi
+
+CLASSPATH=$HADOOP_CONF_DIR
+for file in $SRC_DIR/target/samza/lib/*.jar;
+do
+  CLASSPATH=$CLASSPATH:$file
+done
+
+if [ -z "$JAVA_HOME" ]; then
+  JAVA="java"
+else
+  JAVA="$JAVA_HOME/bin/java"
+fi
+
+JAVA_OPTS="-Dlog4j.configuration=file:$SRC_DIR/target/samza/lib/log4j.xml -Dsamza.log.dir=$SRC_DIR/target"
+configFactory=org.apache.samza.config.factories.PropertiesConfigFactory
+configFile=$SRC_DIR/conf/$benchName.properties
+
+rm -f $SRC_DIR/conf/.properties
+cp $configFile $SRC_DIR/conf/.properties
+cat $SRC_DIR/conf/common.properties >> $SRC_DIR/conf/.properties
+
+echo $JAVA $JAVA_OPTS -cp $CLASSPATH org.apache.samza.job.JobRunner --config-factory=$configFactory --config-path=file://$SRC_DIR/conf/.properties
+exec $JAVA $JAVA_OPTS -cp $CLASSPATH org.apache.samza.job.JobRunner --config-factory=$configFactory --config-path=file://$SRC_DIR/conf/.properties
