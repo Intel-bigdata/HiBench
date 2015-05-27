87a81ccd32bf9e9c8d5da3a6fda1ba497676ea48
Lv, Qi <qi.lv@intel.com>
2015-05-27 14:23:29 +0800

    redirect samza's script & workload improvements

--- src/streambench/stormbench/src/jvm/com/intel/PRCcloud/micro/SampleStream.java
+++ src/streambench/stormbench/src/jvm/com/intel/PRCcloud/micro/SampleStream.java
@@ -9,34 +9,49 @@ import com.intel.PRCcloud.*;
 import com.intel.PRCcloud.util.*;
 import com.intel.PRCcloud.topologies.*;
 
+import java.util.Random;
+
 public class SampleStream extends SingleSpoutTops{
-	
-	public SampleStream(StormBenchConfig config){
-	  super(config);
-	}
-	
-	public void setBolt(TopologyBuilder builder){
-      builder.setBolt("sampleAndPrint",new SampleBolt(config.prob),config.boltThreads).shuffleGrouping("spout");
+
+    public SampleStream(StormBenchConfig config){
+        super(config);
     }
-	
-	public static class SampleBolt extends BaseBasicBolt{
-	  private double probability;
-	  private int count=0;
-	  
-	  public SampleBolt(double prob){
-	    probability=prob;
-	  }
-	  
-	  public void execute(Tuple tuple, BasicOutputCollector collector) {
-	    double randVal = Math.random(); //FIXME, Math.random is thread safe!
-		if(randVal<=probability){
-		  count+=1;
-		  collector.emit(new Values(tuple.getString(0)));
-		  BenchLogUtil.logMsg("   count:"+count);
-		}		  
-	  }
-	  
-	  public void declareOutputFields(OutputFieldsDeclarer ofd){
-	  }
-	}
+
+    public void setBolt(TopologyBuilder builder){
+        builder.setBolt("sampleAndPrint",new SampleBolt(config.prob),config.boltThreads).shuffleGrouping("spout");
+    }
+
+    public static class SampleBolt extends BaseBasicBolt{
+
+        private double probability;
+        private int count=0;
+        private ThreadLocal<Random> rand = null;
+
+        public SampleBolt(double prob){
+            probability=prob;
+            rand = threadRandom(1);
+        }
+
+        public void execute(Tuple tuple, BasicOutputCollector collector) {
+            double randVal = rand.get().nextDouble();
+            if(randVal<=probability){
+                count+=1;
+                collector.emit(new Values(tuple.getString(0)));
+                BenchLogUtil.logMsg("   count:"+count);
+            }
+        }
+
+        public void declareOutputFields(OutputFieldsDeclarer ofd){
+        }
+    }
+
+    public static ThreadLocal<Random> threadRandom(final long seed) {
+        return new ThreadLocal<Random>(){
+            @Override
+            protected Random initialValue() {
+                return new Random(seed);
+            }
+        };
+    }
+
 }
--- workloads/streamingbench/samza/bin/metrics.sh
+++ workloads/streamingbench/samza/bin/metrics.sh
@@ -4,21 +4,22 @@ set -u
 bin=`dirname "$0"`
 bin=`cd "$bin";pwd`
 DIR=`cd $bin/../; pwd`
-. "${DIR}/conf/configure.sh"
+SRC_DIR="$DIR/../../../src/streambench/samza"
+. "${SRC_DIR}/conf/configure.sh"
 echo "=========start samza metrics========="
 
-if [ ! -d $DIR/target ]; then
+if [ ! -d $SRC_DIR/target ]; then
   echo "Please run maven first."
   exit 1
 fi
 
-if [ ! -d $DIR/target/samza ]; then
-  mkdir $DIR/target/samza
-  tar zxf $DIR/target/*.tar.gz -C $DIR/target/samza
+if [ ! -d $SRC_DIR/target/samza ]; then
+  mkdir $SRC_DIR/target/samza
+  tar zxf $SRC_DIR/target/*.tar.gz -C $SRC_DIR/target/samza
 fi
 
 CLASSPATH=$HADOOP_CONF_DIR
-for file in $DIR/target/samza/lib/*.jar;
+for file in $SRC_DIR/target/samza/lib/*.jar;
 do
   CLASSPATH=$CLASSPATH:$file
 done
@@ -29,9 +30,9 @@ else
   JAVA="$JAVA_HOME/bin/java"
 fi
 
-JAVA_OPTS="-Dlog4j.configuration=file:$DIR/target/samza/lib/log4j.xml -Dsamza.log.dir=$DIR/target"
+JAVA_OPTS="-Dlog4j.configuration=file:$SRC_DIR/target/samza/lib/log4j.xml -Dsamza.log.dir=$SRC_DIR/target"
 configFactory=org.apache.samza.config.factories.PropertiesConfigFactory
-configFile=$DIR/conf/metrics.properties
+configFile=$SRC_DIR/conf/metrics.properties
 
 echo $JAVA $JAVA_OPTS -cp $CLASSPATH org.apache.samza.job.JobRunner --config-factory=$configFactory --config-path=file://$configFile
 exec $JAVA $JAVA_OPTS -cp $CLASSPATH org.apache.samza.job.JobRunner --config-factory=$configFactory --config-path=file://$configFile
