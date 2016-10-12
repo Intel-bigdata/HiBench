dd9a9d86286f2d973cfa0818aab32855c1973111
Carson Wang <carson.wang@intel.com>
2016-10-12 10:15:19 +0800

    add license header and fix style

--- sparkbench/graph/src/main/scala/com/intel/hibench/sparkbench/graph/nweight/PregelNWeight.scala
+++ sparkbench/graph/src/main/scala/com/intel/hibench/sparkbench/graph/nweight/PregelNWeight.scala
@@ -1,19 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.intel.hibench.sparkbench.graph.nweight
 
 import scala.collection.JavaConversions._
 import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.rdd.RDD
 import org.apache.spark.HashPartitioner
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.graphx._
 import org.apache.spark.graphx.impl.GraphImpl
 import it.unimi.dsi.fastutil.longs.Long2DoubleOpenHashMap
 
-/** * Compute NWeight for Graph G(V, E) as defined below *     Weight(1)(u, v) = edge(u, v)
- *     Weight(n)(u, v) = Sum (over {x|there are edges (u, x) and (x, v)}) Weight(n-1)(u, x)*Weight(1)(x, v)
+/**
+ * Compute NWeight for Graph G(V, E) as defined below.
  *
- * Input is given in Text file format. Each line represents a Node and all out edges of that node (edge weight specified)
+ * Weight(1)(u, v) = edge(u, v)
+ * Weight(n)(u, v) =
+ *   Sum (over {x|there are edges (u, x) and (x, v)}) Weight(n-1)(u, x) * Weight(1)(x, v)
+ *
+ * Input is given in Text file format. Each line represents a Node and all out edges of that node
+ * (edge weight specified)
  *  <vertex> <vertex1>:<weight1>, <vertex2>:<weight2> ...)
  */
 
@@ -33,7 +53,8 @@ object PregelNWeight extends Serializable{
   def mergMsg(c1: Long2DoubleOpenHashMap, c2: Long2DoubleOpenHashMap) = {
     c2.long2DoubleEntrySet()
       .fastIterator()
-      .foreach(pair => c1.put(pair.getLongKey(), c1.get(pair.getLongKey()) + pair.getDoubleValue()))
+      .foreach(pair =>
+        c1.put(pair.getLongKey(), c1.get(pair.getLongKey()) + pair.getDoubleValue()))
     c1
   }
 
@@ -70,7 +91,8 @@ object PregelNWeight extends Serializable{
 
     var g = GraphImpl(edges, new SizedPriorityQueue(maxDegree), storageLevel, storageLevel).cache()
 
-    g = Pregel(g, new Long2DoubleOpenHashMap, step, EdgeDirection.In)(vProg _, sendMsg _, mergMsg _)
+    g = Pregel(g, new Long2DoubleOpenHashMap, step, EdgeDirection.In)(
+      vProg _, sendMsg _, mergMsg _)
 
     g.vertices.map { case (vid, vdata) => 
       var s = new StringBuilder
--- sparkbench/graph/src/main/scala/com/intel/hibench/sparkbench/graph/nweight/Utils.scala
+++ sparkbench/graph/src/main/scala/com/intel/hibench/sparkbench/graph/nweight/Utils.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.intel.hibench.sparkbench.graph.nweight
 
 import it.unimi.dsi.fastutil.objects.ObjectHeaps
@@ -6,7 +23,19 @@ class SizedPriorityQueue(
   val capacity:Int) extends Traversable[(Long, Double)] with Serializable {
   protected val buf = new Array[(Long, Double)](capacity)
   protected val comparator = new java.util.Comparator[(Long, Double)] with Serializable {
-    override def compare(m1: (Long, Double), m2: (Long, Double)) = if (m1._2 < m2._2) -1 else (if (m1._2 > m2._2) 1 else (if (m1._1 < m2._1) -1 else (if (m1._1 > m2._1) 1 else 0)))
+    override def compare(m1: (Long, Double), m2: (Long, Double)) : Int = {
+      if (m1._2 < m2._2) {
+        -1
+      } else if (m1._2 > m2._2) {
+        1
+      } else if (m1._1 < m2._1) {
+        -1
+      } else if (m1._1 > m2._1) {
+        1
+      } else {
+        0
+      }
+    }
   }
 
   protected var size_ = 0
