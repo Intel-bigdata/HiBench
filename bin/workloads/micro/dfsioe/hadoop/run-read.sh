ffc17336b248d170f9e2108e557c32774a185556
Marshall Mounier <595604512@qq.com>
2016-10-28 14:56:46 +0800

    Fix report behavior bug (#340)

--- bin/workloads/micro/dfsioe/hadoop/run-read.sh
+++ bin/workloads/micro/dfsioe/hadoop/run-read.sh
@@ -21,7 +21,7 @@ workload_config=${root_dir}/conf/workloads/micro/dfsioe.conf
 . "${root_dir}/bin/functions/load-bench-config.sh"
 
 
-enter_bench HadoopDfsioe-read ${workload_config}
+enter_bench HadoopDfsioe-read ${workload_config} ${current_dir}
 show_bannar start
 
 # path check
