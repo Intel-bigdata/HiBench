39ed3ed855a067ddd37bfe77ce1ff6319750a7ce
intelybzhang <739551801@qq.com>
2016-11-04 23:54:46 +0800

    config ssh1

--- travis/configssh.sh
+++ travis/configssh.sh
@@ -1,6 +1,6 @@
 #restart ssh service
 
-ssh-keygen -q -N "" -t rsa -f ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+ssh-keygen -q -N "" -t rsa -f /root/.ssh/id_rsa
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 echo "    StrictHostKeyChecking no                     " >> /etc/ssh/ssh_config
 service ssh restart
