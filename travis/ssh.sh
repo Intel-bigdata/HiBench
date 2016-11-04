889d57d9de0d4c5b56dda8e7f8ea1c12a44347e0
intelybzhang <739551801@qq.com>
2016-11-04 21:07:35 +0800

    test6

--- travis/ssh.sh
+++ travis/ssh.sh
@@ -1,2 +1,4 @@
+ssh-keygen -q -N "" -t rsa -f /root/.ssh/id_rsa
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 echo "    StrictHostKeyChecking no                     " >> /etc/ssh/ssh_config
 service ssh restart
