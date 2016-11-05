60245d05e8d3056669fd2d789acfbd1de71d5ce6
intelybzhang <739551801@qq.com>
2016-11-05 21:14:55 +0800

    config5

--- travis/configssh.sh
+++ travis/configssh.sh
@@ -3,7 +3,4 @@
 ssh-keygen -q -N "" -t rsa -f /root/.ssh/id_rsa
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 echo "    StrictHostKeyChecking no                     " >> /etc/ssh/ssh_config
-#echo "    UserKnownHostsFile /dev/null                 " >> /etc/ssh/ssh_config
-echo $JAVA_HOME
-echo 1234567890
 service ssh restart
