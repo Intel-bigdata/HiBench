8c17cdc535891c4d607f8486d796925b4b13584e
intelybzhang <739551801@qq.com>
2016-11-05 19:11:42 +0800

    config ssh2

--- travis/configssh.sh
+++ travis/configssh.sh
@@ -3,4 +3,5 @@
 ssh-keygen -q -N "" -t rsa -f /root/.ssh/id_rsa
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 echo "    StrictHostKeyChecking no                     " >> /etc/ssh/ssh_config
+echo "    UserKnownHostsFile /dev/null                 " >> /etc/ssh/ssh_config
 service ssh restart
