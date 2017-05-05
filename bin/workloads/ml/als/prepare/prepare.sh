19bf217994282e9abaa3eef7c0964c3a5c73169c
Yinan Xiang <yinan.xiang@intel.com>
2017-05-05 08:44:35 +0800

    [als] Change config name in als (#456)
    Signed-off-by: ynXiang <yinan.xiang@intel.com>

--- bin/workloads/ml/als/prepare/prepare.sh
+++ bin/workloads/ml/als/prepare/prepare.sh
@@ -18,15 +18,15 @@ current_dir=`dirname "$0"`
 current_dir=`cd "$current_dir"; pwd`
 root_dir=${current_dir}/../../../../../
 workload_config=${root_dir}/conf/workloads/ml/als.conf
-. "${root_dir}/bin/functions/load-bench-config.sh"
+. "${root_dir}/bin/functions/load_bench_config.sh"
 
 enter_bench RatingDataPrepare ${workload_config} ${current_dir}
 show_bannar start
 
-rmr-hdfs $INPUT_HDFS || true
+rmr_hdfs $INPUT_HDFS || true
 START_TIME=`timestamp`
 
-run-spark-job com.intel.hibench.sparkbench.ml.RatingDataGenerator $INPUT_HDFS $NUM_USERS $NUM_PRODUCTS $IMPLICITPREFS 
+run_spark_job com.intel.hibench.sparkbench.ml.RatingDataGenerator $INPUT_HDFS $NUM_USERS $NUM_PRODUCTS $IMPLICITPREFS 
 
 END_TIME=`timestamp`
 
