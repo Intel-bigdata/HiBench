2141abe9b9848850fb7af19201630a1e1176b4a2
Carson Wang <carson.wang@intel.com>
2017-06-14 10:02:11 +0800

    Update travis scripts

--- travis/restart_hadoop_spark.sh
+++ travis/restart_hadoop_spark.sh
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 # stop process
-/opt/hadoop-2.6.5/sbin/stop-dfs.sh
-/opt/hadoop-2.6.5/sbin/stop-yarn.sh
-/opt/hadoop-2.6.5/sbin/stop-all.sh
+#/opt/hadoop-2.6.5/sbin/stop-dfs.sh
+#/opt/hadoop-2.6.5/sbin/stop-yarn.sh
+#/opt/hadoop-2.6.5/sbin/stop-all.sh
 
 # clear data directories
 mkdir -p /usr/local/hdfs/namenode/
@@ -37,4 +37,4 @@ rm -fr /opt/hadoop-2.6.5/logs/*
 /opt/hadoop-2.6.5/sbin/start-yarn.sh
 
 # restart spark
-/opt/spark-1.6.0-bin-hadoop2.6/sbin/start-all.sh
+#/opt/spark-1.6.0-bin-hadoop2.6/sbin/start-all.sh
