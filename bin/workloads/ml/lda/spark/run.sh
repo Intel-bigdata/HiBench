3a82203dfd7ce79f7c69c9a7025c2b216fd5fd18
jtengyp <jtengyp@foxmail.com>
2017-07-26 16:34:22 +0800

    Add maxResultSize parameter for LDAExample
    set the spark.driver.maxResultSize

--- bin/workloads/ml/lda/spark/run.sh
+++ bin/workloads/ml/lda/spark/run.sh
@@ -26,7 +26,7 @@ rmr_hdfs $OUTPUT_HDFS || true
 
 SIZE=`dir_size $INPUT_HDFS`
 START_TIME=`timestamp`
-run_spark_job com.intel.hibench.sparkbench.ml.LDAExample $INPUT_HDFS $OUTPUT_HDFS $NUM_TOPICS_LDA
+run_spark_job com.intel.hibench.sparkbench.ml.LDAExample $INPUT_HDFS $OUTPUT_HDFS $NUM_TOPICS_LDA $MAXRESULTSIZE_LDA
 END_TIME=`timestamp`
 
 gen_report ${START_TIME} ${END_TIME} ${SIZE}
