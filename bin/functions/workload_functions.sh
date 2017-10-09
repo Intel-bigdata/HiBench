385dc924c9b331149b59f6188cf1b9322f3e80db
He, Yu <yu.he@intel.com>
2017-10-09 15:49:59 +0800

    Print bench.log
    Signed-off-by: He, Yu <yu.he@intel.com>

--- bin/functions/workload_functions.sh
+++ bin/functions/workload_functions.sh
@@ -228,7 +228,7 @@ function run_spark_job() {
     then
         echo -e "${BRed}ERROR${Color_Off}: Spark job ${BYellow}${CLS}${Color_Off} failed to run successfully."
         echo -e "${BBlue}Hint${Color_Off}: You can goto ${BYellow}${WORKLOAD_RESULT_FOLDER}/bench.log${Color_Off} to check for detailed log.\nOpening log tail for you:\n"
-        tail ${WORKLOAD_RESULT_FOLDER}/bench.log
+        cat ${WORKLOAD_RESULT_FOLDER}/bench.log
         exit $result
     fi
 }
