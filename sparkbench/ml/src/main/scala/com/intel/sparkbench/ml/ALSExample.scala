28329112ad251d084f91b87bb5067d11fdaa66f2
jtengyp <jtengyp@foxmail.com>
2017-10-18 16:40:44 +0800

    Add data sparsity and recommendProductsForUsers

--- sparkbench/ml/src/main/scala/com/intel/sparkbench/ml/ALSExample.scala
+++ sparkbench/ml/src/main/scala/com/intel/sparkbench/ml/ALSExample.scala
@@ -26,7 +26,7 @@ import scopt.OptionParser
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.recommendation.{ALS, MatrixFactorizationModel, Rating}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg.SparseVector
 
 object ALSExample {
 
@@ -34,11 +34,11 @@ object ALSExample {
       dataPath: String = null,
       numUsers: Int = 0,
       numProducts: Int = 0,
-      sparsity: Double = 0.01,
       kryo: Boolean = false,
       numIterations: Int = 20,
       lambda: Double = 1.0,
       rank: Int = 10,
+      numRecommends: Int = 20,
       numUserBlocks: Int = -1,
       numProductBlocks: Int = -1,
       implicitPrefs: Boolean = false)
@@ -54,12 +54,12 @@ object ALSExample {
       opt[Int]("numProducts")
         .text(s"numProducts, default: ${defaultParams.numProducts}")
         .action((x, c) => c.copy(numProducts = x))
-      opt[Double]("sparsity")
-        .text(s"sparsity, default: ${defaultParams.sparsity}")
-        .action((x, c) => c.copy(sparsity = x))
       opt[Int]("rank")
         .text(s"rank, default: ${defaultParams.rank}")
         .action((x, c) => c.copy(rank = x))
+      opt[Int]("numRecommends")
+        .text(s"numRecommends, default: ${defaultParams.numRecommends}")
+        .action((x, c) => c.copy(numRecommends = x))
       opt[Int]("numIterations")
         .text(s"number of iterations, default: ${defaultParams.numIterations}")
         .action((x, c) => c.copy(numIterations = x))
@@ -101,17 +101,17 @@ object ALSExample {
 
     val numUsers = params.numUsers
     val numProducts = params.numProducts
+    val numRecommends = params.numRecommends
     val implicitPrefs = params.implicitPrefs
 
-    val rawdata: RDD[Vector] = sc.objectFile(params.dataPath)
-    val data: RDD[Rating] = Vector2Rating(rawdata, numUsers, numProducts)
-    val training: RDD[Rating] = data.randomSplit(Array(1-params.sparsity, params.sparsity))(0).cache()
-    val testsparsity = 0.75 * (1-params.sparsity)
-    val test: RDD[Rating] = data.randomSplit(Array(1-testsparsity, testsparsity))(0)
+    val rawdata: RDD[SparseVector] = sc.objectFile(params.dataPath)
+    val data: RDD[Rating] = Vector2Rating(rawdata)
+    val splits = data.randomSplit(Array(0.7, 0.3))
+    val (trainingData, testData) = (splits(0), splits(1))
 
-    val numTraining = training.count()
-    val numTest = test.count()
-    println(s"Training: $numTraining, test: $numTest.")
+    val numTraining = trainingData.count()
+    val numTest = testData.count()
+    println(s"Num of Training: $numTraining, Num of Test: $numTest.")
 
     val model = new ALS()
       .setRank(params.rank)
@@ -120,11 +120,15 @@ object ALSExample {
       .setImplicitPrefs(params.implicitPrefs)
       .setUserBlocks(params.numUserBlocks)
       .setProductBlocks(params.numProductBlocks)
-      .run(training)
+      .run(trainingData)
 
-    val rmse = computeRmse(model, test, params.implicitPrefs)
+    val rmse = computeRmse(model, testData, params.implicitPrefs)
 
     println(s"Test RMSE = $rmse.")
+    
+    // Recommend products for all users
+    val userRecommend = model.recommendProductsForUsers(numRecommends)
+    userRecommend.count()
 
     sc.stop()
   }
@@ -144,19 +148,17 @@ object ALSExample {
     math.sqrt(predictionsAndRatings.map(x => (x._1 - x._2) * (x._1 - x._2)).mean())
   }
 
-  def Vector2Rating(rawdata: RDD[Vector], m: Int, n: Int)
-    : RDD[Rating] = {
-    
-    val Ratingdata: RDD[Rating] = rawdata.zipWithIndex().flatMap(v => {
-      var arr = new Array[Rating](n)
-      var i = 0
-      for (i <- 0 until n){
-        arr(i) = Rating(v._2.toInt, i, v._1.apply(i))
-      }
-      arr
-    })
+  def Vector2Rating(rawdata: RDD[SparseVector]) : RDD[Rating] = {
+    val Ratingdata: RDD[Rating] = rawdata.zipWithIndex().flatMap{
+      case(v,i) =>
+        val arr = mutable.ArrayBuilder.make[Rating]
+        arr.sizeHint(v.numActives)
+        v.foreachActive{(ii, vi) =>
+          arr += Rating(i.toInt, ii, vi)
+        }
+        arr.result()
+    }
     Ratingdata
   }
 
 }
-// scalastyle:on println
