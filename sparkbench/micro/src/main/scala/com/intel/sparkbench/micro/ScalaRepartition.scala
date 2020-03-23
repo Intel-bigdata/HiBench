708e8e67a5c8cfad7da94955061e8d2124019ad6
Chenzhao Guo <chenzhao.guo@intel.com>
2020-03-23 18:30:02 +0800

    Save RDDs in reduce stage

--- sparkbench/micro/src/main/scala/com/intel/sparkbench/micro/ScalaRepartition.scala
+++ sparkbench/micro/src/main/scala/com/intel/sparkbench/micro/ScalaRepartition.scala
@@ -17,10 +17,13 @@
 
 package com.intel.hibench.sparkbench.micro
 
+import java.util.Random
+
 import com.intel.hibench.sparkbench.common.IOCommon
 import org.apache.hadoop.examples.terasort.TeraInputFormat
 import org.apache.hadoop.io.Text
 import org.apache.spark._
+import org.apache.spark.rdd.{CoalescedRDD, RDD, ShuffledRDD}
 import org.apache.spark.storage.StorageLevel
 
 object ScalaRepartition {
@@ -51,8 +54,27 @@ object ScalaRepartition {
     val reduceParallelism  = IOCommon.getProperty("hibench.default.shuffle.parallelism")
       .getOrElse((mapParallelism / 2).toString).toInt
 
-    data.repartition(reduceParallelism).foreach(_ => {})
+    reparition(data, reduceParallelism).foreach(_ => {})
 
     sc.stop()
   }
+
+  // Save a CoalescedRDD than RDD.repartition API
+  private def reparition(previous: RDD[Array[Byte]], numReducers: Int): ShuffledRDD[Int, Array[Byte], Array[Byte]] = {
+    /** Distributes elements evenly across output partitions, starting from a random partition. */
+    val distributePartition = (index: Int, items: Iterator[Array[Byte]]) => {
+      var position = (new Random(index)).nextInt(numReducers)
+      items.map { t =>
+        // Note that the hash code of the key will just be the key itself. The HashPartitioner
+        // will mod it with the number of total partitions.
+        position = position + 1
+        (position, t)
+      }
+    } : Iterator[(Int, Array[Byte])]
+
+    // include a shuffle step so that our upstream tasks are still distributed
+    new ShuffledRDD[Int, Array[Byte], Array[Byte]](previous.mapPartitionsWithIndex(distributePartition),
+      new HashPartitioner(numReducers))
+  }
+
 }
