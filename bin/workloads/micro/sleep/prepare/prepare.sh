59bf3d6927c336484982cb83fa3ad43effb0e919
minmingzhu <minming.zhu@intel.com>
2020-07-27 02:50:21 -0400

    update travis
    Signed-off-by: minmingz <minming.zhu@inte.com>

--- bin/workloads/micro/sleep/prepare/prepare.sh
+++ bin/workloads/micro/sleep/prepare/prepare.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+echo "===="${HADOOP_HOME}
 current_dir=`dirname "$0"`
 current_dir=`cd "$current_dir"; pwd`
 root_dir=${current_dir}/../../../../../
