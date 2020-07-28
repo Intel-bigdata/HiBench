65e8b8e9a9b986843f0892a2f78adad54846e7c9
minmingzhu <minming.zhu@intel.com>
2020-07-28 06:44:24 -0400

    update travis
    Signed-off-by: minmingz <minming.zhu@inte.com>

--- travis/restart_hadoop_spark.sh
+++ travis/restart_hadoop_spark.sh
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+echo ${HADOOP_HOME}
+echo ${SPARK_HOME}
+
 # stop process
 ${HADOOP_HOME}/sbin/stop-dfs.sh
 ${HADOOP_HOME}/sbin/stop-yarn.sh
