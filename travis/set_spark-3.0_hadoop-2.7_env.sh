da5d8784bc26241df8e991aa8d1d2513055a9e51
minmingzhu <minming.zhu@intel.com>
2020-07-28 07:19:57 -0400

    update travis
    Signed-off-by: minmingz <minming.zhu@inte.com>

--- travis/set_spark-3.0_hadoop-2.7_env.sh
+++ travis/set_spark-3.0_hadoop-2.7_env.sh
@@ -10,4 +10,3 @@ sed -i '1 i hibench.spark.home /opt/spark-3.0.0-bin-hadoop2.7\nhibench.spark.ver
 sed -i '1 i hibench.hadoop.examples.jar  ${hibench.hadoop.home}/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.7.jar\nhibench.hadoop.examples.test.jar  ${hibench.hadoop.home}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.7.7-tests.jar' ./conf/hibench.conf
 echo "export HADOOP_HOME=/opt/hadoop-2.7.7" >> ~/.branchrc
 echo "export SPARK_HOME=/opt/spark-3.0.0-bin-hadoop2.7" >> ~/.branchrc
-source ~/.branchrc
