de25a76b071a585cbd1548cc183a94387211f15e
minmingzhu <minming.zhu@intel.com>
2020-07-28 04:46:23 -0400

    update travis
    Signed-off-by: minmingz <minming.zhu@inte.com>

--- travis/configssh.sh
+++ travis/configssh.sh
@@ -1,7 +1,7 @@
+#!/usr/bin/env bash
 #restart ssh service
 
 ssh-keygen -q -N "" -t rsa -f /root/.ssh/id_rsa
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 echo "    StrictHostKeyChecking no                     " >> /etc/ssh/ssh_config
 service ssh restart
-echo "hadoop home : "+$HADOOP_HOME
