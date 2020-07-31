4888c15bde5c5a9c044975aef53e3a2f1b4d4b5d
Xiaochang Wu <xiaochang.wu@intel.com>
2020-07-31 09:05:36 +0800

    Add params for new linear regression (#635)
    * Add params for linear
    
    * fix bugs
    
    * Update linear.conf

--- bin/workloads/ml/linear/spark/run.sh
+++ bin/workloads/ml/linear/spark/run.sh
@@ -26,7 +26,9 @@ rmr_hdfs $OUTPUT_HDFS || true
 
 SIZE=`dir_size $INPUT_HDFS`
 START_TIME=`timestamp`
-run_spark_job com.intel.hibench.sparkbench.ml.LinearRegressionWithElasticNet ${INPUT_HDFS}
+run_spark_job com.intel.hibench.sparkbench.ml.LinearRegressionWithElasticNet \
+    --regParam ${REG_PARAM_LINEAR} --elasticNetParam ${ELASTICNET_PARAM_LINEAR} \
+    --maxIter ${NUM_ITERATIONS_LINEAR} --tol ${TOL_LINEAR} ${INPUT_HDFS}
 END_TIME=`timestamp`
 
 gen_report ${START_TIME} ${END_TIME} ${SIZE}
