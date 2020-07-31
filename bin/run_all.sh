4a351a5da1845ff2ca0230af6945ef4d661194e3
Wu, Xiaochang <xiaochang.wu@intel.com>
2020-07-31 21:47:45 +0800

    nit

--- bin/run_all.sh
+++ bin/run_all.sh
@@ -35,36 +35,36 @@ for benchmark in `cat $root_dir/conf/benchmarks.lst`; do
     result=$?
     if [ $result -ne 0 ]
     then
-	echo "ERROR: ${benchmark} prepare failed!"
+    echo "ERROR: ${benchmark} prepare failed!"
         exit $result
     fi
 
     for framework in `cat $root_dir/conf/frameworks.lst`; do
-	if [[ $framework == \#* ]]; then
-	    continue
-	fi
+    if [[ $framework == \#* ]]; then
+        continue
+    fi
 
-	if [ $benchmark == "micro/dfsioe" ] && [ $framework == "spark" ]; then
-	    continue
-	fi
-	if [ $benchmark == "micro/repartition" ] && [ $framework == "hadoop" ]; then
-	    continue
-	fi
-	if [ $benchmark == "websearch/nutchindexing" ] && [ $framework == "spark" ]; then
-	    continue
-	fi
-	if [ $benchmark == "graph/nweight" ] && [ $framework == "hadoop" ]; then
-	    continue
-	fi
-	if [ $benchmark == "ml/lr" ] && [ $framework == "hadoop" ]; then
-	    continue
-	fi
-	if [ $benchmark == "ml/als" ] && [ $framework == "hadoop" ]; then
-	    continue
-	fi
-	if [ $benchmark == "ml/svm" ] && [ $framework == "hadoop" ]; then
-	    continue
-	fi
+    if [ $benchmark == "micro/dfsioe" ] && [ $framework == "spark" ]; then
+        continue
+    fi
+    if [ $benchmark == "micro/repartition" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "websearch/nutchindexing" ] && [ $framework == "spark" ]; then
+        continue
+    fi
+    if [ $benchmark == "graph/nweight" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "ml/lr" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "ml/als" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "ml/svm" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
     if [ $benchmark == "ml/pca" ] && [ $framework == "hadoop" ]; then
         continue
     fi
@@ -83,20 +83,20 @@ for benchmark in `cat $root_dir/conf/benchmarks.lst`; do
     if [ $benchmark == "ml/lda" ] && [ $framework == "hadoop" ]; then
         continue
     fi
-  	if [ $benchmark == "ml/gmm" ] && [ $framework == "hadoop" ]; then
-	    continue
-	fi
+    if [ $benchmark == "ml/gmm" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
 
-	echo -e "${UYellow}${BYellow}Run ${Yellow}${UYellow}${benchmark}/${framework}${Color_Off}"
-	echo -e "${BCyan}Exec script: ${Cyan}$WORKLOAD/${framework}/run.sh${Color_Off}"
-	$WORKLOAD/${framework}/run.sh
+    echo -e "${UYellow}${BYellow}Run ${Yellow}${UYellow}${benchmark}/${framework}${Color_Off}"
+    echo -e "${BCyan}Exec script: ${Cyan}$WORKLOAD/${framework}/run.sh${Color_Off}"
+    $WORKLOAD/${framework}/run.sh
 
-	result=$?
-	if [ $result -ne 0 ]
-	then
-	    echo -e "${On_IRed}ERROR: ${benchmark}/${framework} failed to run successfully.${Color_Off}"
+    result=$?
+    if [ $result -ne 0 ]
+    then
+        echo -e "${On_IRed}ERROR: ${benchmark}/${framework} failed to run successfully.${Color_Off}"
             exit $result
-	fi
+    fi
     done
 done
 
