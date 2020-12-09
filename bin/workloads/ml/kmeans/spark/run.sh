d1eb83e850e09754c31b167a5f5500c329aba167
minmingzhu <minming.zhu@intel.com>
2020-12-09 19:20:30 +0800

    1. set initmode to Random 2. add initmode as an adjustable parameter to kmeans.conf and run.sh
    Signed-off-by: minmingz <minming.zhu@inte.com>

--- bin/workloads/ml/kmeans/spark/run.sh
+++ bin/workloads/ml/kmeans/spark/run.sh
@@ -28,7 +28,7 @@ rmr_hdfs $OUTPUT_HDFS || true
 SIZE=`dir_size $INPUT_HDFS`
 START_TIME=`timestamp`
 
-run_spark_job com.intel.hibench.sparkbench.ml.DenseKMeans -k $K --numIterations $MAX_ITERATION --storageLevel $K_STORAGE_LEVEL $INPUT_HDFS/samples
+run_spark_job com.intel.hibench.sparkbench.ml.DenseKMeans -k $K --numIterations $MAX_ITERATION --storageLevel $K_STORAGE_LEVEL --initMode $K_INIT_MODE $INPUT_HDFS/samples
 END_TIME=`timestamp`
 
 gen_report ${START_TIME} ${END_TIME} ${SIZE}
