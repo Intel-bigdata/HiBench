57249311241235a731f73d14cf938f9029c71ddd
minmingzhu <minmingzhu@intel.com>
2021-09-07 15:28:21 +0800

    update correlationExample
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- sparkbench/ml/src/main/scala/com/intel/sparkbench/ml/CorrelationExample.scala
+++ sparkbench/ml/src/main/scala/com/intel/sparkbench/ml/CorrelationExample.scala
@@ -1,22 +1,17 @@
 package com.intel.hibench.sparkbench.ml
 
 import org.apache.spark.ml.feature.LabeledPoint
-import org.apache.spark.ml.linalg.Vector
 import scopt.OptionParser
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.ml.linalg.{Matrix, Vector}
 import org.apache.spark.ml.stat.Correlation
-import org.apache.spark.ml.linalg.Matrix
-import org.apache.spark.sql.Row
-
 
 object CorrelationExample {
 
   case class Params(
        input: String = null,
        corrType: String = "pearson",
-       fracTest: Double = 0.25
        )
 
   def main(args: Array[String]): Unit = {
@@ -29,21 +24,10 @@ object CorrelationExample {
         .text(s"String specifying the method to use for computing correlation. " +
           s"Supported: `pearson` (default), `spearman`, default: ${defaultParams.corrType}")
         .action((x, c) => c.copy(corrType = x))
-      opt[Double]("fracTest")
-        .text(s"fraction of data to hold out for testing. If given option testInput, " +
-          s"this option is ignored. default: ${defaultParams.fracTest}")
-        .action((x, c) => c.copy(fracTest = x))
       arg[String]("<input>")
         .text("input path to labeled examples")
         .required()
         .action((x, c) => c.copy(input = x))
-      checkConfig { params =>
-        if (params.fracTest < 0 || params.fracTest >= 1) {
-          failure(s"fracTest ${params.fracTest} value incorrect; should be in [0,1).")
-        } else {
-          success
-        }
-      }
     }
 
     parser.parse(args, defaultParams) match {
@@ -53,17 +37,14 @@ object CorrelationExample {
   }
 
   def run(params: Params): Unit = {
-    val conf = new SparkConf().setAppName(s"Correlations with $params")
-    val sc = new SparkContext(conf)
     val spark = SparkSession
       .builder
+      .appName(s"Correlations with $params")
       .getOrCreate()
 
-    val training: DataFrame = loadDatasets(params.input,"regression",params.fracTest)
-
-
-    println(s"Summary of data file: ${params.input}")
-    println(s"${training.count()} data points")
+    val data: RDD[LabeledPoint] = spark.sparkContext.objectFile(params.input)
+    import spark.implicits._
+    val training = data.toDF().cache()
 
 
     val numTraining = training.count()
@@ -72,35 +53,15 @@ object CorrelationExample {
     println(s"  numTraining = $numTraining")
     println(s"  numFeatures = $numFeatures")
 
-    val corrType = "pearson"
 
-    println(s"Correlation ($corrType) between label and each feature")
+    println(s"Correlation ${params.corrType} between label and each feature")
     println(s"Feature\tCorrelation")
+
     val df = training.toDF("label", "features")
-    val Row(coeff: Matrix) = Correlation.corr(df, "features", params.corrType).head
-    println(s"Correlation matrix:\n $coeff")
+    val Row(coeff1: Matrix) = Correlation.corr(df, "features", params.corrType)
+    println(s"Pearson correlation matrix:\n $coeff1.")
 
-    sc.stop()
+    spark.stop()
   }
 
-  private[ml] def loadDatasets(input: String,
-                               algo: String,
-                               fracTest: Double): DataFrame ={
-    println("Loaded data:")
-    val spark = SparkSession
-      .builder
-      .getOrCreate()
-
-    // Load training data
-    val data: RDD[LabeledPoint] = spark.sparkContext.objectFile(input)
-    import spark.implicits._
-    val origExamples = data.toDF()
-
-    // Load or create test set
-    val dataframes: Array[DataFrame] = origExamples.randomSplit(Array(1.0 - fracTest, fracTest), seed = 12345)
-
-    val training = dataframes(0).cache()
-
-    training
-  }
 }
