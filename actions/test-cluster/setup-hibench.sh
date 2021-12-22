83557d8c958e754565d85a9a589a38e38c6f501d
minmingzhu <minming.zhu@intel.com>
2021-12-22 22:40:52 +0800

    update
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

--- actions/test-cluster/setup-hibench.sh
+++ actions/test-cluster/setup-hibench.sh
@@ -9,13 +9,13 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # copy hibench conf
-cp $GITHUB_WORKSPACE/actions/hibench.conf $GITHUB_WORKSPACE//conf/
-cp $GITHUB_WORKSPACE/actions/spark.conf $GITHUB_WORKSPACE//conf/
-cp $GITHUB_WORKSPACE//actions/hadoop.conf $GITHUB_WORKSPACE//conf/
+cp $GITHUB_WORKSPACE/actions/hibench.conf $GITHUB_WORKSPACE/conf/
+cp $GITHUB_WORKSPACE/actions/spark.conf $GITHUB_WORKSPACE/conf/
+cp $GITHUB_WORKSPACE//actions/hadoop.conf $GITHUB_WORKSPACE/conf/
 
 # set hadoop path , spark path and dependency jar
-sed -i '1 i hibench.hadoop.home /opt/hadoop-2.7.7' ./conf/hadoop.conf
-sed -i '1 i hibench.spark.home /opt/spark-2.4.0-bin-hadoop2.7\nhibench.spark.version spark2.4' ./conf/spark.conf
-sed -i '1 i hibench.hadoop.examples.jar  ${hibench.hadoop.home}/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.7.jar\nhibench.hadoop.examples.test.jar  ${hibench.hadoop.home}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.7.7-tests.jar\nhibench.hive.release		apache-hive-0.14.0-bin' ./conf/hibench.conf
+sed -i '1 i hibench.hadoop.home ~/opt/hadoop-2.7.7' ./conf/hadoop.conf
+sed -i '1 i hibench.spark.home ~/opt/spark-2.4.0-bin-hadoop2.7\nhibench.spark.version spark2.4' ./conf/spark.conf
+sed -i '1 i hibench.hadoop.examples.jar  ${hibench.hadoop.home}/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.7.jar\nhibench.hadoop.examples.test.jar  ${hibench.hadoop.home}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.7.7-tests.jar\nhibench.hive.release		apache-hive-0.14.0-bin' $GITHUB_WORKSPACE/conf/hibench.conf
 
 set +x
