0d0c34ca6db076302a35ab237c8b1e687aca6065
minmingzhu <45281494+minmingzhu@users.noreply.github.com>
2021-12-23 13:34:07 +0800

    Update setup-hibench.sh

--- actions/test-cluster/setup-hibench.sh
+++ actions/test-cluster/setup-hibench.sh
@@ -18,7 +18,7 @@ cp $GITHUB_WORKSPACE//actions/hadoop.conf $GITHUB_WORKSPACE/conf/
 
 # set hadoop path , spark path and dependency jar
 sed -i '1 i hibench.hadoop.home ~/opt/hadoop-2.7.7' $GITHUB_WORKSPACE/conf/hadoop.conf
-sed -i '1 i hhibench.hdfs.master hdfs://"${HOST_NAME}":8020' $GITHUB_WORKSPACE/conf/hadoop.conf
+sed -i "1 i hhibench.hdfs.master hdfs://${HOST_NAME}:8020" $GITHUB_WORKSPACE/conf/hadoop.conf
 cat $GITHUB_WORKSPACE/conf/hadoop.conf
 sed -i '1 i hibench.spark.home ~/opt/spark-2.4.0-bin-hadoop2.7\nhibench.spark.version spark2.4' $GITHUB_WORKSPACE/conf/spark.conf
 sed -i '1 i hibench.hadoop.examples.jar  ${hibench.hadoop.home}/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.7.jar\nhibench.hadoop.examples.test.jar  ${hibench.hadoop.home}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.7.7-tests.jar\nhibench.hive.release		apache-hive-0.14.0-bin' $GITHUB_WORKSPACE/conf/hibench.conf
