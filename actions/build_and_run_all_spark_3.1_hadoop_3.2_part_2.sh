c6f51e6ea97336ca000ccf77a58a8260a45845fe
minmingzhu <minming.zhu@intel.com>
2023-07-17 03:23:49 +0000

    split benchmark list
    Signed-off-by: minmingzhu <minming.zhu@intel.com>

---
+++ actions/build_and_run_all_spark_3.1_hadoop_3.2_part_2.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# exit when any command fails
+set -e
+
+export HADOOP_VERSION=3.2.1
+export SPARK_VERSION=3.1.1
+export SPARK_BIN_VERSION=spark3.1
+export SPARK_HADOOP_VERSION=hadoop3.2
+export HIVE_VERSION=3.0.0
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+# mvn build
+mvn clean package -q -Dmaven.javadoc.skip=true -Dspark=3.1 -Dscala=2.12
+
+# Setup cluster contain of hadoop and spark
+source $GITHUB_WORKSPACE/actions/test-cluster/setup-cluster.sh
+
+#Setup Hibench
+source $GITHUB_WORKSPACE/actions/test-cluster/setup-hibench.sh
+
+echo "========================================="
+echo "Cluster Testing with Spark Version: $SPARK_VERSION"
+echo "========================================="
+
+# run all examples
+source $GITHUB_WORKSPACE/bin/run_all_part_2.sh
---
+++ bin/run_all_part_1.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -u
+
+current_dir=`dirname "$0"`
+root_dir=`cd "${current_dir}/.."; pwd`
+
+. ${root_dir}/bin/functions/color.sh
+
+for benchmark in `cat $root_dir/conf/benchmarks_part_1.lst`; do
+    if [[ $benchmark == \#* ]]; then
+        continue
+    fi
+
+    echo -e "${UYellow}${BYellow}Prepare ${Yellow}${UYellow}${benchmark} ${BYellow}...${Color_Off}"
+    benchmark="${benchmark/.//}"
+
+    WORKLOAD=$root_dir/bin/workloads/${benchmark}
+    echo -e "${BCyan}Exec script: ${Cyan}${WORKLOAD}/prepare/prepare.sh${Color_Off}"
+    "${WORKLOAD}/prepare/prepare.sh"
+
+    result=$?
+    if [ $result -ne 0 ]
+    then
+    echo "ERROR: ${benchmark} prepare failed!"
+        exit $result
+    fi
+
+    for framework in `cat $root_dir/conf/frameworks.lst`; do
+    if [[ $framework == \#* ]]; then
+        continue
+    fi
+
+    if [ $benchmark == "micro/dfsioe" ] && [ $framework == "spark" ]; then
+        continue
+    fi
+    if [ $benchmark == "micro/repartition" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "websearch/nutchindexing" ] && [ $framework == "spark" ]; then
+        continue
+    fi
+    if [ $benchmark == "graph/nweight" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "graph/pagerank" ] && [ $framework == "hadoop" ]; then
+	    continue
+	  fi
+    if [ $benchmark == "ml/lr" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "ml/als" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "ml/svm" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "ml/pca" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "ml/gbt" ] && [ $framework == "hadoop" ]; then
+         continue
+    fi
+    if [ $benchmark == "ml/rf" ] && [ $framework == "hadoop" ]; then
+          continue
+    fi  
+    if [ $benchmark == "ml/svd" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi      
+    if [ $benchmark == "ml/linear" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "ml/lda" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "ml/gmm" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "ml/correlation" ] && [ $framework == "hadoop" ]; then
+        continue
+    fi
+    if [ $benchmark == "ml/summarizer" ] && [ $framework == "hadoop" ]; then
+         continue
+    fi
+
+    echo -e "${UYellow}${BYellow}Run ${Yellow}${UYellow}${benchmark}/${framework}${Color_Off}"
+    echo -e "${BCyan}Exec script: ${Cyan}$WORKLOAD/${framework}/run.sh${Color_Off}"
+    $WORKLOAD/${framework}/run.sh
+
+    result=$?
+    if [ $result -ne 0 ]
+    then
+        echo -e "${On_IRed}ERROR: ${benchmark}/${framework} failed to run successfully.${Color_Off}"
+            exit $result
+    fi
+    done
+done
+
+echo "Run all done!"
